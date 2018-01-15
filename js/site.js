37e2190d4f114f84769ee760fb46de4d6464c380
Bilal Karim <beekay-@users.noreply.github.com>
2018-01-14 20:24:34 -0700

    Added hero images for mobile

--- js/site.js
+++ js/site.js
@@ -8,6 +8,14 @@ var heroBgList = ['hero-1', 'hero-2', 'hero-3'];
 var randomHeroBg = heroBgList[Math.floor(Math.random()*heroBgList.length)];
 document.getElementById('hero').classList = randomHeroBg;
 
+// Mobile images
+var imgs = document.getElementsByClassName('mobile');
+for (var i = 0; i < imgs.length; i++) {
+  var num = Math.floor(Math.random() * 3 + 1);
+  imgs[i].src = 'img/hero-' + num + '-sm.jpg';
+  imgs[i].alt = imgs[i].src;
+}
+
 // Reviews from Google and Facebook
 var reviewsArray = [];
 var siteReviews = {
